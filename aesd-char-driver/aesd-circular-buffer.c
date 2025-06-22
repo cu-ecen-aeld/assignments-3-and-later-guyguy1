96abfba5f539ba85a52c9119b9ca7a0a225d7e85
Guy Levanon <guylevanon1@gmail.com>
2025-06-22 12:00:22 +0300

    Implement circular buffer

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -12,9 +12,30 @@
 #include <linux/string.h>
 #else
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 #endif
 
 #include "aesd-circular-buffer.h"
+
+#define BUFFER_SIZE AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED
+
+
+static uint8_t get_number_of_entries(struct aesd_circular_buffer *buffer)
+{
+    if (buffer->in_offs > buffer->out_offs)
+    {
+        return buffer->in_offs - buffer->out_offs;
+    }
+    else if (buffer->in_offs < buffer->out_offs)
+    {
+        return (BUFFER_SIZE - buffer->out_offs + buffer->in_offs);
+    }
+    else
+    {
+        return buffer->full ? BUFFER_SIZE : 0;
+    }
+}
 
 /**
  * @param buffer the buffer to search for corresponding offset.  Any necessary locking must be performed by caller.
@@ -29,9 +50,22 @@
 struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct aesd_circular_buffer *buffer,
             size_t char_offset, size_t *entry_offset_byte_rtn )
 {
-    /**
-    * TODO: implement per description
-    */
+    uint8_t num_entries = get_number_of_entries(buffer);
+
+    for (int i = 0; i < num_entries; ++i)
+    {
+        uint8_t entry_index = (buffer->out_offs + i) % BUFFER_SIZE;
+        struct aesd_buffer_entry *entry = &buffer->entry[entry_index];
+
+        if (entry->size > char_offset)
+        {
+            *entry_offset_byte_rtn = char_offset;
+            return entry;
+        }
+
+        char_offset -= entry->size;
+    }
+    
     return NULL;
 }
 
@@ -44,9 +78,37 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
 */
 void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
 {
-    /**
-    * TODO: implement per description
-    */
+    if (buffer->full && buffer->in_offs != buffer->out_offs)
+    {
+        printf("ERROR: buffer is marked full but in_offs = %d, out_offs = %d\n", buffer->in_offs, buffer->out_offs);
+        exit(-1);
+    }
+
+    buffer->entry[buffer->in_offs].buffptr = add_entry->buffptr;
+    buffer->entry[buffer->in_offs].size = add_entry->size;
+
+    buffer->in_offs++;
+
+    // Check wrap around
+    if (buffer->in_offs > BUFFER_SIZE)
+    {
+        printf("ERROR: buffer->in_offs is too big (%d)\n", buffer->in_offs);
+        exit(-1);
+    }
+    else if (buffer->in_offs == BUFFER_SIZE)
+    {
+        buffer->in_offs = 0;
+    }
+
+    // handle buffer->full
+    if (buffer->full)
+    {
+        buffer->out_offs = buffer->in_offs;
+    }
+    else if (buffer->in_offs == buffer->out_offs)
+    {
+        buffer->full = true;
+    }
 }
 
 /**
